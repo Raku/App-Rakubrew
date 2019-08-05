299d00439b9e8faf705a87d45f26e879276f2f3b
Patrick Böker <patzim@users.noreply.github.com>
2019-08-05 21:45:27 +0200

    WIP PowerShell support

--- lib/Rakudobrew/ShellHook/PowerShell.pm
+++ lib/Rakudobrew/ShellHook/PowerShell.pm
@@ -4,7 +4,7 @@ our @ISA = "Rakudobrew::ShellHook";
 use strict;
 use warnings;
 use 5.010;
-use File::Spec::Functions qw(catdir splitpath);
+use File::Spec::Functions qw(catfile catdir splitpath);
 use FindBin qw($RealBin $RealScript);
 
 use Rakudobrew::Variables;
@@ -17,52 +17,87 @@ use Rakudobrew::Build;
 # https://superuser.com/questions/886951/run-powershell-script-when-you-open-powershell
 # https://www.computerperformance.co.uk/powershell/profile-ps1/
 
-sub supports_hooking {
-    my $self = shift;
-    0;
-}
-
-sub install_note {
-    return <<EOT;
-To install $brew_name permanently type the following into your terminal
-(only works with an administrative console window):
-  [Environment]::SetEnvironmentVariable("PATH", "$RealBin;$shim_dir;\$Env:PATH", "User")
-To make use of $brew_name in this session only use
-  \$Env:PATH = "$RealBin;$shim_dir;\$Env:PATH"
-
-Using the GUI:
-Start -> right click on "Computer" -> Properties -> (Advanced system settings)
--> Advanced -> Environment Variables... ->  System variables
--> select PATH -> Edit... -> prepend "$RealBin;$shim_dir;"
-
+=pod
 WARNING:
 Setting PATH to a string longer than 2048 chars (4096 on newer systems) can cause the
 PATH to be truncated, your PATH being set to the empty string and only become available
 again upon reboot and in the worst case cause your system to not boot anymore.
 See https://web.archive.org/web/20190519191717/https://software.intel.com/en-us/articles/limitation-to-the-length-of-the-system-path-variable
-EOT
+
+This problem is smaller for us, because we only modify PATH in the current console, never globally.
+=cut
+
+sub supports_hooking {
+    my $self = shift;
+    1;
 }
 
-sub shell_setenv_msg {
-    my $self = shift;
+sub install_note {
+    my $brew_exec = catfile($RealBin, $brew_name);
     return <<EOT;
-To (un)set a version locally in this running shell session use the following commands:
-\$Env:$env_var="YOUR_VERSION" # set
-Remove-Item Env:\\$env_var    # unset
+Load $brew_name automatically by adding
+
+  perl $brew_exec init PowerShell | Out-String | Invoke-Expression
+
+to your PowerShell profile.
+This can be easily done using:
+
+  Add-Content -Force -Path \$PROFILE -Value 'perl $brew_exec init PowerShell | Out-String | Invoke-Expression'
 EOT
 }
 
 sub get_init_code {
     my $self = shift;
     my $path = $ENV{PATH};
-    $path = Rakudobrew::ShellHook::clean_path($path, $RealBin);
-    $path = "$RealBin:$path";
-    $path = join(':', $shim_dir, $path);
+    $path = $self->clean_path($path, $RealBin);
+    $path = "$RealBin;$path";
+    if (get_brew_mode() eq 'env') {
+        if (get_global_version() && get_global_version() ne 'system') {
+            $path = join(';', get_bin_paths(get_global_version()), $path);
+        }
+    }
+    else { # get_brew_mode() eq 'shim'
+        $path = join(';', $shim_dir, $path);
+    }
+    
+    my $brew_exec = catfile($RealBin, $brew_name);
+    
     return <<EOT;
-setenv PATH "$path"
+\$Env:PATH = "$path"
+Function $brew_name {
+    # TODO: abort if first command fails.
+    perl $brew_exec internal_hooked PowerShell \$args
+    if (\$LASTEXITCODE -ne 0) {
+        Throw "Rakudobrew failed with exitcode \$LASTEXITCODE"
+    }
+    \$cmd = perl $brew_exec internal_shell_hook PowerShell post_call_eval \$args | Out-String
+    if (\$cmd) {
+        Invoke-Expression -Command \$cmd
+    }
+}
 EOT
+}
 
+sub post_call_eval {
+    my $self = shift;
+    $self->print_shellmod_code(@_);
+}
+
+sub get_path_setter_code {
+    my $self = shift;
+    my $path = shift;
+    return "\$Env:PATH = \"$path\"";
+}
+
+sub get_shell_setter_code {
+    my $self    = shift;
+    my $version = shift;
+    return "Set-Variable -Name $env_var -Value \"$version\" -Scope Global";
+}
+
+sub get_shell_unsetter_code {
+    my $self = shift;
+    return "Remove-Variable -Name $env_var -Scope Global";
 }
 
 1;
-
