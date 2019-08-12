b2355e38e4fdb4bea32d23ccd9978eed1b04734d
Patrick Böker <patzim@users.noreply.github.com>
2019-08-12 14:42:16 +0200

    Implement CMD shell hooking

--- lib/Rakudobrew/ShellHook/Cmd.pm
+++ lib/Rakudobrew/ShellHook/Cmd.pm
@@ -1,10 +1,10 @@
-package Rakudobrew::ShellHook::PowerShell;
+package Rakudobrew::ShellHook::Cmd;
 use Rakudobrew::ShellHook;
 our @ISA = "Rakudobrew::ShellHook";
 use strict;
 use warnings;
 use 5.010;
-use File::Spec::Functions qw(catdir splitpath);
+use File::Spec::Functions qw(catfile catdir splitpath);
 use FindBin qw($RealBin $RealScript);
 
 use Rakudobrew::Variables;
@@ -16,49 +16,93 @@ use Rakudobrew::Build;
 
 sub supports_hooking {
     my $self = shift;
-    0;
+    1;
 }
 
 sub install_note {
+    my $brew_exec = catfile($RealBin, $brew_name);
     return <<EOT;
-To install $brew_name permanently type the following into your terminal:
-  SETX PATH "$RealBin;$shim_dir;%PATH%"
-To make use of $brew_name in this session only use
-  SET PATH=$RealBin;$shim_dir;%PATH%
+To load $brew_name in CMD automatically you have to do two things:
 
-Using the GUI:
-Start -> right click on "Computer" -> Properties -> (Advanced system settings)
--> Advanced -> Environment Variables... ->  System variables
--> select PATH -> Edit... -> prepend "$RealBin;$shim_dir;"
+1. Check that you don't already have a CMD autorun script set.
 
-WARNING:
-Setting PATH to a string longer than 2048 chars (4096 on newer systems) can cause the
-PATH to be truncated, your PATH being set to the empty string and only become available
-again upon reboot and in the worst case cause your system to not boot anymore.
-See https://web.archive.org/web/20190519191717/https://software.intel.com/en-us/articles/limitation-to-the-length-of-the-system-path-variable
-EOT
-}
+    reg query "HKCU\\Software\\Microsoft\\Command Processor" /v AutoRun
 
-sub shell_setenv_msg {
-    my $self = shift;
-    return <<EOT;
-To (un)set a version locally in this running shell session use the following commands:
-\$Env:$env_var="YOUR_VERSION" # set
-Remove-Item Env:\\$env_var    # unset
+  If you don't have an autorun script set (the above command returns an error) you can set one using:
+
+    reg add "HKCU\\Software\\Microsoft\\Command Processor" /v AutoRun /t REG_EXPAND_SZ /d \\""\%"USERPROFILE"\%\\Documents\\CMD_profile.cmd"\\" /f
+
+2. Add the following code to the end of the autorun script you linked in step 1:
+
+    \@echo off
+    setlocal EnableDelayedExpansion
+    set "cmd=!cmdcmdline!"
+    if "!cmd!" == "!cmd:/=!" (
+        endlocal
+        FOR /f "delims=" \%\%i in ('perl C:\\data\\rakudobrew\\bin\\rakudobrew init Cmd') do \@\%\%i
+    )
+
+  You can easily do that from a CMD prompt using the following command:
+
+    (
+    echo \@echo off
+    echo setlocal EnableDelayedExpansion
+    echo set "cmd=!cmdcmdline!"
+    echo if "!cmd!" == "!cmd:/=!" ^(
+    echo     endlocal
+    echo     FOR /f "delims=" \%\%i in ^('perl C:\\data\\rakudobrew\\bin\\rakudobrew init Cmd'^) do \@\%\%i
+    echo ^)
+    ) >> "\%USERPROFILE\%\\Documents\\CMD_profile.cmd"
+
+  If you use a different autorun script location replace the path in the command above.
 EOT
 }
 
 sub get_init_code {
     my $self = shift;
     my $path = $ENV{PATH};
-    $path = Rakudobrew::ShellHook::clean_path($path, $RealBin);
-    $path = "$RealBin:$path";
-    $path = join(':', $shim_dir, $path);
-    return <<EOT;
-setenv PATH "$path"
-EOT
+    $path = $self->clean_path($path, $RealBin);
+    $path = "$RealBin;$path";
+    if (get_brew_mode() eq 'env') {
+        if (get_global_version() && get_global_version() ne 'system') {
+            $path = join(';', get_bin_paths(get_global_version()), $path);
+        }
+    }
+    else { # get_brew_mode() eq 'shim'
+        $path = join(';', $shim_dir, $path);
+    }
 
+    my $brew_exec = catfile($RealBin, $brew_name);
+    # https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/doskey
+    # https://devblogs.microsoft.com/oldnewthing/20120731-00/?p=7003
+    # The command that post_call_eval() returns is always a single line, so we can get away with having an empty delimiter.
+    # The second for is there to not error on empty lines: https://stackoverflow.com/a/31316333
+    return <<EOT;
+SET PATH=$path
+doskey rakudobrew=perl $brew_exec internal_hooked Cmd \$* && FOR /f "delims=" \%i in ('perl $brew_exec internal_shell_hook Cmd post_call_eval \$*') do \@\%i
+EOT
+}
+
+sub post_call_eval {
+    my $self = shift;
+    $self->print_shellmod_code(@_);
+}
+
+sub get_path_setter_code {
+    my $self = shift;
+    my $path = shift;
+    return "SET PATH=$path";
+}
+
+sub get_shell_setter_code {
+    my $self    = shift;
+    my $version = shift;
+    return "SET $env_var=$version"
+}
+
+sub get_shell_unsetter_code {
+    my $self = shift;
+    return "UNSET $env_var";
 }
 
 1;
-
