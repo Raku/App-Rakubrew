3e98a10e91012da8f0e1182bd070f948c1e9cada
Patrick Böker <patzim@users.noreply.github.com>
2020-02-01 16:17:28 +0100

    Hopefully fix manual builds + test of better artifact upload
    Currently artifacts are somehow compressed a second time seemingly
    automatically by circleci. Maybe not having the .tgz ending in
    destination prevents this double compression.

--- release-stuff/trigger-manual-build.sh
+++ release-stuff/trigger-manual-build.sh
@@ -2,17 +2,19 @@
 
 if [ $# -lt 1 ]; then
     echo 'You need to pass:'
+    echo '   - a version'
     echo '   - a CircleCI token'
     exit 1
 fi
 
 curl \
--u $1: \
+-u $2: \
 -X POST \
 -H 'Content-Type: application/json' \
 -d "{
   \"parameters\": {
-    \"MANUAL_BUILD\": true
+    \"MANUAL_BUILD\": true,
+    \"VERSION\": \"$2\"
   }
 }" \
 https://circleci.com/api/v2/project/gh/Raku/App-Rakubrew/pipeline
