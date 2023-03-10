6e91baa9c8aa65cfd22e31965b3a5c1c9d8e702f
Patrick Böker <patrick.boeker@posteo.de>
2023-03-10 12:51:00 +0100

    Automate building rakubrew on ARM MacOS

--- release-stuff/trigger-manual-build.sh
+++ release-stuff/trigger-manual-build.sh
@@ -1,4 +1,41 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+# Sourced from https://stackoverflow.com/a/29835459/1975049
+rreadlink() (
+  target=$1 fname= targetDir= CDPATH=
+  { \unalias command; \unset -f command; } >/dev/null 2>&1
+  [ -n "$ZSH_VERSION" ] && options[POSIX_BUILTINS]=on
+  while :; do
+      [ -L "$target" ] || [ -e "$target" ] || { command printf '%s\n' "ERROR: '$target' does not exist." >&2; return 1; }
+      command cd "$(command dirname -- "$target")" || exit 1
+      fname=$(command basename -- "$target")
+      [ "$fname" = '/' ] && fname=''
+      if [ -L "$fname" ]; then
+        target=$(command ls -l "$fname")
+        target=${target#* -> }
+        continue
+      fi
+      break
+  done
+  targetDir=$(command pwd -P)
+  if [ "$fname" = '.' ]; then
+    command printf '%s\n' "${targetDir%/}"
+  elif  [ "$fname" = '..' ]; then
+    command printf '%s\n' "$(command dirname -- "${targetDir}")"
+  else
+    command printf '%s\n' "${targetDir%/}/$fname"
+  fi
+)
+
+EXEC=$(rreadlink "$0")
+DIR=$(dirname -- "$EXEC")
+
+
+# ============================================
+# Actual script starts here
 
 if [ $# -lt 2 ]; then
     echo 'You need to pass:'
@@ -19,3 +56,10 @@ curl \
 }" \
 https://circleci.com/api/v2/project/gh/Raku/App-Rakubrew/pipeline
 
+# Do the MacOS Arm build
+REMOTE_PATH=/Users/administrator/repos/App-Rakubrew
+ssh administrator@207.254.31.127 "\
+    rm $REMOTE_PATH/rakubrew;\
+    $REMOTE_PATH/release-stuff/build-macos.sh"
+
+scp administrator@207.254.31.127:$REMOTE_PATH/rakubrew $DIR/../rakubrew_macos_arm
